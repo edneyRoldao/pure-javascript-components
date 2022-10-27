2a313b4ccc17059eab50d63bdcd3e3cdf5037ce6
edneyRoldao <edneyroldao@gmail.com>
2022-10-26 23:57:19 -0300

    modal

--- src/components/modal/script.js
+++ src/components/modal/script.js
@@ -1,6 +1,36 @@
-const openModalBtn = document.querySelector('#modal')
-const overlay = document.querySelector('#overlay')
+const styles = `
+    ._modal_overlay {
+        position: fixed;
+        opacity: 0;
+        transition: 600ms ease-in-out;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: rgba(0, 0, 0, .5);
+        pointer-events: none;
+    }
+
+    ._modal_overlay.active {
+        pointer-events: all;
+        opacity: 1;
+    }
+`
+
+const styleElement = document.createElement('style')
+styleElement.innerText = styles
+document.head.appendChild(styleElement)
+
+const modalOverlay = document.createElement('div')
+modalOverlay.classList.add('_modal_overlay')
+
+const body = document.querySelector('body')
+body.appendChild(modalOverlay)
+
+
+// Open modal listener
+const openModalBtn = document.querySelector('#openModalBtn')
 
 openModalBtn.addEventListener('click', () => {
-    overlay.classList.add('active')
+    modalOverlay.classList.add('active')
 })
