549247b9c05071e288ec5c5cc6f3d3f1944b3a47
edneyRoldao <edneyroldao@gmail.com>
2023-07-10 10:10:41 -0300

    modal

--- src/components/modal-loading/script.js
+++ src/components/modal-loading/script.js
@@ -2,6 +2,12 @@
 const modalOverlay = document.querySelector('._modal_overlay')
 const openModalBtn = document.querySelector('#openModalBtn')
 
+
+function closeModal() {
+    modalOverlay.classList.remove('active')
+}
+
 openModalBtn.addEventListener('click', () => {
     modalOverlay.classList.add('active')
+    setTimeout(closeModal, 1000);
 })
